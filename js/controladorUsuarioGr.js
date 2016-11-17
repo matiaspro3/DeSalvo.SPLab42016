29f9b5bc630cb6ccae36d2d50271f36924ec9ccc
matiaspro3 <matias9387@hotmail.com>
2016-11-17 20:20:52 -0300

    perfiles

--- js/controladorUsuarioGr.js
+++ js/controladorUsuarioGr.js
@@ -1,9 +1,9 @@
 angular.module('abmapp.filtrosUsuarioGrilla', []) 
   app.filter('tipo', function () {
    var perfil = {
-      'admin': 'Administrador',
-      'vend': 'Vendedor',
-      'comp': 'Comprador'
+      'administrador': 'Administrador',
+      'vendedor': 'Vendedor',
+      'comprador': 'Comprador'
     }
    return function (input) {
         if (!input)
